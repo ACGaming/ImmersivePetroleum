e2bddc650a0fd04dbd9b1277ad22eea9b82abfd1
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2020-11-18 23:24:29 +0100

    Projector ui texture changed to fit IE's general style more
    Texture was made by KitKat and just *slightly* modified by me

--- src/main/java/flaxbeard/immersivepetroleum/client/gui/ProjectorScreenOld.java
+++ src/main/java/flaxbeard/immersivepetroleum/client/gui/ProjectorScreenOld.java
@@ -38,7 +38,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
-import net.minecraft.util.IReorderingProcessor;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Quaternion;
 import net.minecraft.util.math.vector.Vector3i;
@@ -50,7 +49,7 @@ import net.minecraftforge.client.model.data.EmptyModelData;
 import net.minecraftforge.client.model.data.IModelData;
 import net.minecraftforge.common.util.Lazy;
 
-public class ProjectorScreen extends Screen{
+public class ProjectorScreenOld extends Screen{
 	static final ResourceLocation GUI_TEXTURE = new ResourceLocation(MODID, "textures/gui/projector.png");
 	
 	static final ITextComponent GUI_CONFIRM = translation("gui.immersivepetroleum.projector.button.confirm");
@@ -61,6 +60,8 @@ public class ProjectorScreen extends Screen{
 	static final ITextComponent GUI_UP = translation("gui.immersivepetroleum.projector.button.up");
 	static final ITextComponent GUI_DOWN = translation("gui.immersivepetroleum.projector.button.down");
 	static final ITextComponent GUI_SEARCH = translation("gui.immersivepetroleum.projector.search");
+	
+	private Minecraft mc = Minecraft.getInstance();
 	
 	private int xSize = 256;
 	private int ySize = 166;
@@ -77,44 +78,39 @@ public class ProjectorScreen extends Screen{
 	Settings settings;
 	Hand hand;
 	
-	float rotation = 0.0F, move = 0.0F;
-	public ProjectorScreen(Hand hand, ItemStack projector){
+	public ProjectorScreenOld(Hand hand, ItemStack projector){
 		super(new StringTextComponent("projector"));
 		this.settings = new Settings(projector);
 		this.hand = hand;
 		this.multiblocks = Lazy.of(() -> MultiblockHandler.getMultiblocks());
-		
-		if(this.settings.getMultiblock() != null){
-			this.move = 20F;
-		}
 	}
 	
 	@Override
 	protected void init(){
-		this.width = Minecraft.getInstance().getMainWindow().getScaledWidth();
-		this.height = Minecraft.getInstance().getMainWindow().getScaledHeight();
+		this.width = this.mc.getMainWindow().getScaledWidth();
+		this.height = this.mc.getMainWindow().getScaledHeight();
 		
 		this.guiLeft = (this.width - this.xSize) / 2;
 		this.guiTop = (this.height - this.ySize) / 2;
 		
-		this.searchField = addButton(new SearchField(this.font, this.guiLeft + 25, this.guiTop + 13));
+		this.searchField = addButton(new SearchField(this.font, this.guiLeft + 23, this.guiTop + 11));
 		
-		addButton(new ConfirmButton(this.guiLeft + 115, this.guiTop + 10, but -> {
+		addButton(new ConfirmButton(this.guiLeft + 132, this.guiTop + 5, but -> {
 			ItemStack held = Minecraft.getInstance().player.getHeldItem(this.hand);
 			this.settings.applyTo(held);
 			this.settings.sendPacketToServer(this.hand);
 			Minecraft.getInstance().currentScreen.closeScreen();
 		}));
-		addButton(new CancelButton(this.guiLeft + 115, this.guiTop + 34, but -> {
+		addButton(new CancelButton(this.guiLeft + 152, this.guiTop + 5, but -> {
 			Minecraft.getInstance().currentScreen.closeScreen();
 		}));
-		addButton(new MirrorButton(this.guiLeft + 115, this.guiTop + 58, this.settings, but -> {
+		addButton(new MirrorButton(this.guiLeft + 172, this.guiTop + 5, this.settings, but -> {
 			this.settings.flip();
 		}));
-		addButton(new RotateLeftButton(this.guiLeft + 115, this.guiTop + 106, but -> {
+		addButton(new RotateLeftButton(this.guiLeft + 192, this.guiTop + 5, but -> {
 			this.settings.rotateCCW();
 		}));
-		addButton(new RotateRightButton(this.guiLeft + 115, this.guiTop + 130, but -> {
+		addButton(new RotateRightButton(this.guiLeft + 232, this.guiTop + 5, but -> {
 			this.settings.rotateCW();
 		}));
 		
@@ -146,7 +142,7 @@ public class ProjectorScreen extends Screen{
 			IMultiblock mb = this.multiblocks.get().get(Integer.valueOf(str));
 			String name;
 			if(mb instanceof UnionMultiblock && mb.getUniqueName().getPath().contains("excavator_demo")){
-				name = I18n.format("desc.immersiveengineering.info.multiblock.IE:Excavator") + "2";
+				name = I18n.format("desc.immersiveengineering.info.multiblock.IE:Excavator")+"2";
 			}else{
 				name = I18n.format("desc.immersiveengineering.info.multiblock.IE:" + ProjectorItem.getActualMBName(mb));
 			}
@@ -155,12 +151,12 @@ public class ProjectorScreen extends Screen{
 		});
 		
 		this.listEntries = list.toArray(new String[0]);
-		GuiReactiveList guilist = new GuiReactiveList(this, this.guiLeft + 15, this.guiTop + 29, 89, 127, button -> listaction(button), this.listEntries);
+		GuiReactiveList guilist = new GuiReactiveList(this, this.guiLeft + 7, this.guiTop + 26, 100, 133, button -> listaction(button), this.listEntries);
 		guilist.setPadding(1, 1, 1, 1);
 		guilist.setTranslationFunc(str -> {
 			IMultiblock mb = this.multiblocks.get().get(Integer.valueOf(str));
 			if(mb instanceof UnionMultiblock && mb.getUniqueName().getPath().contains("excavator_demo")){
-				return I18n.format("desc.immersiveengineering.info.multiblock.IE:Excavator") + "2";
+				return I18n.format("desc.immersiveengineering.info.multiblock.IE:Excavator")+"2";
 			}
 			return I18n.format("desc.immersiveengineering.info.multiblock.IE:" + ProjectorItem.getActualMBName(mb));
 		});
@@ -177,34 +173,9 @@ public class ProjectorScreen extends Screen{
 		if(b != -1) this.children.set(b, this.list);
 	}
 	
+	float rotation = 0.0F;
 	@Override
 	public void render(MatrixStack matrix, int mouseX, int mouseY, float partialTicks){
-		// Over-GUI Text
-		if(this.settings.getMultiblock() != null){
-			IMultiblock mb = this.settings.getMultiblock();
-			int x = this.guiLeft + 28;
-			int y = this.guiTop - (int) (15 * (this.move / 20F));
-			
-			if(this.move < 20){
-				this.move += 1.5 * partialTicks;
-			}
-			
-			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-			Minecraft.getInstance().getTextureManager().bindTexture(GUI_TEXTURE);
-			blit(matrix, x, y, 0, 166, 200, 13);
-			
-			ITextComponent text;
-			if(mb instanceof UnionMultiblock && this.settings.getMultiblock().getUniqueName().getPath().contains("excavator_demo")){
-				text = new TranslationTextComponent("desc.immersiveengineering.info.multiblock.IE:Excavator").appendString("2");
-			}else{
-				text = new TranslationTextComponent("desc.immersiveengineering.info.multiblock.IE:" + ProjectorItem.getActualMBName(mb));
-			}
-			
-			x += 100;
-			y += 3;
-			IReorderingProcessor re = text.func_241878_f();
-			this.font.func_238422_b_(matrix, re, (x - this.font.func_243245_a(re) / 2), y, 0x3F3F3F);
-		}
 		background(matrix, mouseX, mouseY, partialTicks);
 		super.render(matrix, mouseX, mouseY, partialTicks);
 		this.searchField.render(matrix, mouseX, mouseY, partialTicks);
@@ -216,22 +187,33 @@ public class ProjectorScreen extends Screen{
 			}
 		}
 		
-		{ // Direction Display (N-S-E-W)
-			int x = this.guiLeft + 115;
-			int y = this.guiTop + 82;
+		{
+			int x = this.guiLeft + 212;
+			int y = this.guiTop + 5;
+			
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			Minecraft.getInstance().getTextureManager().bindTexture(GUI_TEXTURE);
+			blit(matrix, x, y, 18, 166, 18, 18);
 			
 			Direction dir = Direction.byHorizontalIndex(this.settings.getRotation().ordinal());
-			StringTextComponent dirText = new StringTextComponent(dir.toString().toUpperCase().substring(0, 1));
-			drawCenteredString(matrix, this.font, dirText, x + 5, y + 1, -1);
+			drawCenteredString(matrix, this.font, new StringTextComponent(dir.toString().toUpperCase().substring(0, 1)), x + 9, y + 5, -1);
 			
-			if(mouseX > x && mouseX < x + 10 && mouseY > y && mouseY < y + 10){
-				ITextComponent rotText = new TranslationTextComponent("desc.immersivepetroleum.info.projector.rotated." + dir);
-				renderTooltip(matrix, rotText, mouseX, mouseY);
+			if(mouseX > x && mouseX < x + 18 && mouseY > y && mouseY < y + 18){
+				ITextComponent text = new TranslationTextComponent("desc.immersivepetroleum.info.projector.rotated." + dir);
+				renderTooltip(matrix, text, mouseX, mouseY);
 			}
 		}
 		
+		
 		if(this.settings.getMultiblock() != null){
 			IMultiblock mb = this.settings.getMultiblock();
+			ITextComponent text;
+			if(mb instanceof UnionMultiblock && this.settings.getMultiblock().getUniqueName().getPath().contains("excavator_demo")){
+				text = new TranslationTextComponent("desc.immersiveengineering.info.multiblock.IE:Excavator").appendString("2");
+			}else{
+				text = new TranslationTextComponent("desc.immersiveengineering.info.multiblock.IE:" + ProjectorItem.getActualMBName(mb));
+			}
+			drawCenteredString(matrix, this.font, text, this.guiLeft + 127, this.guiTop - 10, -1);
 			
 			IRenderTypeBuffer.Impl buffer = IRenderTypeBuffer.getImpl(Tessellator.getInstance().getBuffer());
 			try{
@@ -241,10 +223,10 @@ public class ProjectorScreen extends Screen{
 				Vector3i size = mb.getSize(null);
 				matrix.push();
 				{
-					matrix.translate(this.guiLeft + 190, this.guiTop + 80, 64);
+					matrix.translate(this.guiLeft + 190, this.guiTop + 90, 64);
 					matrix.scale(mb.getManualScale(), -mb.getManualScale(), 1);
 					matrix.rotate(new Quaternion(25, 0, 0, true));
-					matrix.rotate(new Quaternion(0, (int) (45 - this.rotation), 0, true));
+					matrix.rotate(new Quaternion(0, 45-(int)rotation, 0, true));
 					matrix.translate(size.getX() / -2F, size.getY() / -2F, size.getZ() / -2F);
 					
 					boolean tempDisable = true;
@@ -255,7 +237,7 @@ public class ProjectorScreen extends Screen{
 						}
 						matrix.pop();
 					}else{
-						if(this.blockAccess == null || (this.blockAccess.getMultiblock().getUniqueName().equals(mb.getUniqueName()))){
+						if(this.blockAccess==null || (this.blockAccess.getMultiblock().getUniqueName().equals(mb.getUniqueName()))){
 							this.blockAccess = MultiblockProjection.getBlockAccessFor(mb);
 						}
 						
@@ -311,55 +293,48 @@ public class ProjectorScreen extends Screen{
 	
 	// CLASSES
 	
-	class ConfirmButton extends ProjectorScreen.ControlButton{
+	class ConfirmButton extends ProjectorScreenOld.ControlButton{
 		public ConfirmButton(int x, int y, Consumer<PButton> action){
-			super(x, y, 10, 10, 0, 179, action, GUI_CONFIRM);
+			super(x, y, 18, 18, 19, 185, action, GUI_CONFIRM);
 		}
 	}
 	
-	class CancelButton extends ProjectorScreen.ControlButton{
+	class CancelButton extends ProjectorScreenOld.ControlButton{
 		public CancelButton(int x, int y, Consumer<PButton> action){
-			super(x, y, 10, 10, 10, 179, action, GUI_CANCEL);
+			super(x, y, 18, 18, 37, 185, action, GUI_CANCEL);
 		}
 	}
 	
-	class MirrorButton extends ProjectorScreen.ControlButton{
+	class MirrorButton extends ProjectorScreenOld.ControlButton{
 		Settings settings;
 		public MirrorButton(int x, int y, Settings settings, Consumer<PButton> action){
-			super(x, y, 10, 10, 20, 179, action, GUI_MIRROR);
+			super(x, y, 18, 18, 1, 185, action, GUI_MIRROR);
 			this.settings = settings;
 		}
 		
-		@SuppressWarnings("deprecation")
 		@Override
-		public void renderButton(MatrixStack matrix, int mouseX, int mouseY, float partialTicks){
-			Minecraft.getInstance().getTextureManager().bindTexture(GUI_TEXTURE);
-			RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-			if(isHovered()){
-				fill(matrix, this.x, this.y + 1, this.x + this.iconSize, this.y + this.iconSize - 1, 0xAF7F7FFF);
-			}
-			
+		protected void buttonOverlay(MatrixStack matrix){
+			int vOffset = this.yOverlay;
 			if(this.settings.isMirrored()){
-				blit(matrix, this.x, this.y, this.xOverlay, this.yOverlay + this.iconSize, this.iconSize, this.iconSize);
-			}else{
-				blit(matrix, this.x, this.y, this.xOverlay, this.yOverlay, this.iconSize, this.iconSize);
+				vOffset += this.height;
 			}
+			blit(matrix, this.x + 1, this.y + 1, this.xOverlay, vOffset, this.iconSize, this.iconSize);
 		}
 	}
 	
-	class RotateLeftButton extends ProjectorScreen.ControlButton{
-		public RotateLeftButton(int x, int y, Consumer<PButton> action){
-			super(x, y, 10, 10, 30, 179, action, GUI_ROTATE_CCW);
-		}
-	}
-	
-	class RotateRightButton extends ProjectorScreen.ControlButton{
+	class RotateRightButton extends ProjectorScreenOld.ControlButton{
 		public RotateRightButton(int x, int y, Consumer<PButton> action){
-			super(x, y, 10, 10, 40, 179, action, GUI_ROTATE_CW);
+			super(x, y, 18, 18, 55, 185, action, GUI_ROTATE_CW);
 		}
 	}
 	
-	class ControlButton extends ProjectorScreen.PButton{
+	class RotateLeftButton extends ProjectorScreenOld.ControlButton{
+		public RotateLeftButton(int x, int y, Consumer<PButton> action){
+			super(x, y, 18, 18, 73, 185, action, GUI_ROTATE_CCW);
+		}
+	}
+	
+	class ControlButton extends ProjectorScreenOld.SpriteButton{
 		ITextComponent hoverText;
 		public ControlButton(int x, int y, int width, int height, int overlayX, int overlayY, Consumer<PButton> action, ITextComponent hoverText){
 			super(x, y, width, height, overlayX, overlayY, action);
@@ -368,15 +343,15 @@ public class ProjectorScreen extends Screen{
 		
 		@Override
 		public void renderToolTip(MatrixStack matrixStack, int mouseX, int mouseY){
-			if(this.hoverText != null){
-				ProjectorScreen.this.renderTooltip(matrixStack, this.hoverText, mouseX, mouseY);
+			if(this.hoverText!=null){
+				ProjectorScreenOld.this.renderTooltip(matrixStack, this.hoverText, mouseX, mouseY);
 			}
 		}
 	}
 	
 	class SearchField extends TextFieldWidget{
 		public SearchField(FontRenderer font, int x, int y){
-			super(font, x, y, 60, 14, GUI_SEARCH); // Font, x, y, width, height, tooltip
+			super(font, x, y, 77, 14, GUI_SEARCH);
 			setMaxStringLength(50);
 			setEnableBackgroundDrawing(false);
 			setVisible(true);
@@ -388,7 +363,7 @@ public class ProjectorScreen extends Screen{
 			String s = getText();
 			if(super.keyPressed(keyCode, scanCode, modifiers)){
 				if(!Objects.equals(s, getText())){
-					ProjectorScreen.this.updatelist();
+					ProjectorScreenOld.this.updatelist();
 				}
 				
 				return true;
@@ -407,7 +382,7 @@ public class ProjectorScreen extends Screen{
 			String s = getText();
 			if(super.charTyped(codePoint, modifiers)){
 				if(!Objects.equals(s, getText())){
-					ProjectorScreen.this.updatelist();
+					ProjectorScreenOld.this.updatelist();
 				}
 				
 				return true;
@@ -419,34 +394,19 @@ public class ProjectorScreen extends Screen{
 	
 	// STATIC METHODS
 	
-	static ITextComponent translation(String key){
-		return new TranslationTextComponent(key);
+	static ITextComponent translation(String key, Object... args){
+		return new TranslationTextComponent(key, args);
 	}
 	
 	// STATIC CLASSES
 	
-	static class PButton extends AbstractButton{
+	abstract static class PButton extends AbstractButton{
 		protected boolean selected;
-		protected final int xOverlay, yOverlay;
-		protected int iconSize = 10;
 		protected int bgStartX = 0, bgStartY = 166;
 		protected Consumer<PButton> action;
-		public PButton(int x, int y, int width, int height, int overlayX, int overlayY, Consumer<PButton> action){
+		public PButton(int x, int y, int width, int height, Consumer<PButton> action){
 			super(x, y, width, height, StringTextComponent.EMPTY);
 			this.action = action;
-			this.xOverlay = overlayX;
-			this.yOverlay = overlayY;
-		}
-		
-		@SuppressWarnings("deprecation")
-		@Override
-		public void renderButton(MatrixStack matrix, int mouseX, int mouseY, float partialTicks){
-			Minecraft.getInstance().getTextureManager().bindTexture(GUI_TEXTURE);
-			RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-			if(isHovered()){
-				fill(matrix, this.x, this.y + 1, this.x + this.iconSize, this.y + this.iconSize - 1, 0xAF7F7FFF);
-			}
-			blit(matrix, this.x, this.y, this.xOverlay, this.yOverlay, this.iconSize, this.iconSize);
 		}
 		
 		@Override
@@ -454,12 +414,47 @@ public class ProjectorScreen extends Screen{
 			this.action.accept(this);
 		}
 		
+		@SuppressWarnings("deprecation")
+		@Override
+		public void renderButton(MatrixStack matrix, int mouseX, int mouseY, float partialTicks){
+			Minecraft.getInstance().getTextureManager().bindTexture(GUI_TEXTURE);
+			RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+			int i = this.bgStartX;
+			if(!this.active){
+				i += this.width * 2;
+			}else if(this.selected){
+				i += this.width * 1;
+			}else if(isHovered()){
+				i += this.width * 3;
+			}
+			
+			blit(matrix, this.x, this.y, i, this.bgStartY, this.width, this.height);
+			buttonOverlay(matrix);
+		}
+		
+		protected abstract void buttonOverlay(MatrixStack matrix);
+		
 		public boolean isSelected(){
 			return this.selected;
 		}
 		
 		public void setSelected(boolean isSelected){
 			this.selected = isSelected;
+		}
+	}
+	
+	abstract static class SpriteButton extends ProjectorScreenOld.PButton{
+		protected int iconSize = 16;
+		protected final int xOverlay, yOverlay;
+		public SpriteButton(int x, int y, int width, int height, int overlayX, int overlayY, Consumer<PButton> action){
+			super(x, y, width, height, action);
+			this.xOverlay = overlayX;
+			this.yOverlay = overlayY;
+		}
+		
+		@Override
+		protected void buttonOverlay(MatrixStack matrix){
+			blit(matrix, this.x + 1, this.y + 1, this.xOverlay, this.yOverlay, this.iconSize, this.iconSize);
 		}
 	}
 }
